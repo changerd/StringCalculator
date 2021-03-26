34f8074aae0c7c5e001f7c12a6e3627df77fa094
Slava Morza <bloodreek@gmail.com>
2021-03-26 11:53:18 +0300

    initial calc class and write functions for prior and execop

--- CalculatorLibrary/Calculator.cs
+++ CalculatorLibrary/Calculator.cs
@@ -12,5 +12,69 @@ namespace CalculatorLibrary
         {
             return 0;
         }
+
+        static void ClosingParanthesis(Stack<double> vStack, Stack<char> opStack)
+        {
+
+        }
+
+        static double InputNumebr(char[] exp, int pos, Stack<double> vStack)
+        {
+            return pos;
+        }
+
+        static void InputOpertor(char op, Stack<double> vStack, Stack<char> opStack)
+        {
+
+        }
+
+        static bool OperatorCausesEvalation(char op, char prevOp)
+        {            
+            bool evaluate = false;
+
+            switch(op)
+            {
+                case '+':
+                case '-':
+                    evaluate = (prevOp != '(');
+                    break;
+                case '*':
+                case '/':
+                    evaluate = (prevOp == '*' || prevOp == '/');
+                    break;
+                case ')':
+                    evaluate = true;
+                    break;
+            }
+
+            return evaluate;
+        }
+
+        static void ExecuteOperation(Stack<double> vStack, Stack<char> opStack)
+        {
+            double rightOperand = vStack.Pop();
+            double leftOperand = vStack.Pop();
+            char op = opStack.Pop();
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = leftOperand + rightOperand;
+                    break;
+                case '-':
+                    result = leftOperand - rightOperand;
+                    break;
+                case '*':
+                    result = leftOperand * rightOperand;
+                    break;
+                case '/':
+                    result = leftOperand / rightOperand;
+                    break;
+            }
+
+            vStack.Push(result);
+        }
     }
 }
